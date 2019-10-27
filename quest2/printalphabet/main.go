ddf36e4573a3b27731e529ca2c47d9a61bf2babd
kuat-madeniyet <kuat.madeniyet@gmail.com>
2019-10-28 02:18:00 +0600

    quest2

---
+++ quest2/printalphabet/main.go
@@ -0,0 +1,8 @@
+package main
+import "github.com/01-edu/z01"
+func main() {
+	for i := 97; i < 123; i++ {
+		z01.PrintRune(rune(i))
+	}
+	z01.PrintRune(10)
+}
---
+++ quest2/printreversealphabet/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	z01.PrintRune(122)
+	z01.PrintRune(121)
+	z01.PrintRune(120)
+	z01.PrintRune(119)
+	z01.PrintRune(118)
+	z01.PrintRune(117)
+	z01.PrintRune(116)
+	z01.PrintRune(115)
+	z01.PrintRune(114)
+	z01.PrintRune(113)
+	z01.PrintRune(112)
+	z01.PrintRune(111)
+	z01.PrintRune(110)
+	z01.PrintRune(109)
+	z01.PrintRune(108)
+	z01.PrintRune(107)
+	z01.PrintRune(106)
+	z01.PrintRune(105)
+	z01.PrintRune(104)
+	z01.PrintRune(103)
+	z01.PrintRune(102)
+	z01.PrintRune(101)
+	z01.PrintRune(100)
+	z01.PrintRune(99)
+	z01.PrintRune(98)
+	z01.PrintRune(97)
+	z01.PrintRune(10)
+}
